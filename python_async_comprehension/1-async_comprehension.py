7bc0ca2832fdb68799a0b29dceb5f184ebef6499
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-28 17:26:31 +0200

    task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,9 +4,12 @@ Async Generator
 """
 
 
+import typing
+
+
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[float]:
+async def async_comprehension() -> typing.List[float]:
     """Async Comprehension that collects 10 random numbers"""
     return [i async for i in async_generator()]
