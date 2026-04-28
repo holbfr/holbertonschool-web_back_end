e2f694e1b72ea8e9e8f354f0e540cb2b18d1e586
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-28 16:13:17 +0200

    update task 1 variables annotation

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,10 +5,11 @@ Concurrent coroutines example.
 
 
 import asyncio
+import typing
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> list[float]:
+async def wait_n(n: int, max_delay: int = 10) -> typing.List[float]:
     """Wait for n random delays and return them as a list in ASC order."""
     delays = [wait_random(max_delay) for _ in range(n)]
     result = await asyncio.gather(*delays)
