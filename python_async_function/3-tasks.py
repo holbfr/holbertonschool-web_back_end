c536048536b8f4b67469372d4d41e7d549bf2bf8
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-28 16:32:00 +0200

    task 3 done

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -10,5 +10,4 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """Return a asyncio.Task."""
-    # return asyncio.create_task(wait_random(max_delay))
-    return wait_random(max_delay)
+    return asyncio.create_task(wait_random(max_delay))
