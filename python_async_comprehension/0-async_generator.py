26208e5d09db114fe83bf151c822d46c25a2f360
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-28 17:17:14 +0200

    update return value annotation

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,11 +6,10 @@ Async Generator
 
 import asyncio
 import random
-import typing
-from types import NoneType
+from typing import Generator
 
 
-async def async_generator() -> typing.Generator[float, NoneType, NoneType]:
+async def async_generator() -> Generator[float, None, None]:
     """Async Generator function that takes no arguments"""
     for _ in range(10):
         await asyncio.sleep(1)
