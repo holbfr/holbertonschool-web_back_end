e64ee080c7364fdd8733297f59dd56d12a14b8ab
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-28 17:21:24 +0200

    update task 1 return value annotation

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,6 +7,6 @@ Async Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> list[float]:
     """Async Comprehension that collects 10 random numbers"""
     return [i async for i in async_generator()]
