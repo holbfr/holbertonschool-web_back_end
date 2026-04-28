3912538e9e071b78cdb25f6ab9ff2c3cb48500d8
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-04-28 16:06:41 +0200

    task 1 done

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Concurrent coroutines example.
+"""
+
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int = 10) -> list:
+    """Wait for n random delays and return them as a list in ASC order."""
+    delays = [wait_random(max_delay) for _ in range(n)]
+    result = await asyncio.gather(*delays)
+    return sorted(result)
